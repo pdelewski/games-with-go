f6e763078bba45ff78b73a2206ed4d69b3e47f60
Przemek Delewski <pdelewski@sumologic.com>
2022-07-03 13:53:29 +0200

    fixing dependencies

--- 24_movement/main.go
+++ 24_movement/main.go
@@ -3,11 +3,13 @@ package main
 // https://youtu.be/Jy919y3ezOI?t=1346
 
 import (
+	"github.com/pdelewski/autotel/rtlib"
 	"github.com/pdelewski/games-with-go/24_movement/game"
 	"github.com/pdelewski/games-with-go/24_movement/ui2d"
 )
 
 func main() {
+	rtlib.SumoAutoInstrument()
 	ui := &ui2d.UI2d{}
 	game.Run(ui)
 }
